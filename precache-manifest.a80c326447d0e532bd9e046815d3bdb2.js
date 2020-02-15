5adf9c15960c2c1c17863b598617db13061662d5
Michael <michael@pixt.us>
2020-02-15 20:08:15 +0700

    Updates

---
+++ precache-manifest.a80c326447d0e532bd9e046815d3bdb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2625ef70ac80a757ce9f609bddd275",
+    "url": "/orangeshark/index.html"
+  },
+  {
+    "revision": "c8f207734b3c568d5262",
+    "url": "/orangeshark/static/css/main.ba6308fb.chunk.css"
+  },
+  {
+    "revision": "7ef6fe35af9a8f222182",
+    "url": "/orangeshark/static/js/2.f4f91176.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/orangeshark/static/js/2.f4f91176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f207734b3c568d5262",
+    "url": "/orangeshark/static/js/main.51226a1b.chunk.js"
+  },
+  {
+    "revision": "a0d9b1b743bd74baf0c0",
+    "url": "/orangeshark/static/js/runtime-main.553a247f.js"
+  },
+  {
+    "revision": "73583fa8dab6f29ca80fd47940ed10d6",
+    "url": "/orangeshark/static/media/image1.73583fa8.jpg"
+  }
+]);
