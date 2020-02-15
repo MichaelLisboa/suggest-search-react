d6a782dbab44e44bae02bfc14ceac11d619bf1c1
Michael <michael@pixt.us>
2020-02-15 20:19:21 +0700

    Updates

---
+++ precache-manifest.210b242d89cc6c0c16a5939a64274f73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90706b6fc181fabc205711c710794727",
+    "url": "/suggest-search-react/index.html"
+  },
+  {
+    "revision": "d711f7c03998b683a5da",
+    "url": "/suggest-search-react/static/css/main.ba6308fb.chunk.css"
+  },
+  {
+    "revision": "7ef6fe35af9a8f222182",
+    "url": "/suggest-search-react/static/js/2.f4f91176.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/suggest-search-react/static/js/2.f4f91176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d711f7c03998b683a5da",
+    "url": "/suggest-search-react/static/js/main.94104ee7.chunk.js"
+  },
+  {
+    "revision": "992d22bead3403b8e8e6",
+    "url": "/suggest-search-react/static/js/runtime-main.ddf77397.js"
+  },
+  {
+    "revision": "e6e8d4f3ac504d8435856d8d37bcb4f6",
+    "url": "/suggest-search-react/static/media/image1.e6e8d4f3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suggest-search-react/precache-manifest.545421987876516aa84056f57e74ac39.js"
+  "/suggest-search-react/precache-manifest.210b242d89cc6c0c16a5939a64274f73.js"
 );
 
 self.addEventListener('message', (event) => {
