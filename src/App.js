c1e42377fbef5c4f14db6835909a653d3438ae44
Michael <michael@pixt.us>
2020-02-15 18:06:30 +0700

    random updates for article

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Autocomplete from "./Autocomplete";
 import {countriesList} from "./Countries";
 
+import image from "./image1.jpg"
+
 function App() {
   return (
-    <section className="uk-section uk-section-primary uk-flex uk-flex-top uk-flex-center" data-uk-height-viewport>
-        <div className="uk-padding-small uk-width-1-2@m">
-            <h3>Start typing a country name</h3>
+    <section
+        style={{backgroundImage: `url(${image})`}}
+        className="uk-section uk-background-cover uk-background-primary uk-light uk-flex uk-flex-center uk-background-blend-multiply"
+        data-uk-height-viewport>
+        <div className="uk-padding uk-width-1-2@m">
+            <h1>Where in the world<br />have you been?</h1>
             <Autocomplete countries={countriesList} />
         </div>
     </section>
--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useRef, useReducer } from "react";
 
 const Autocomplete = props => {
 
-    const fieldRef = useRef();
+    const items = props.countries; // This could be a GET request to whatever
+
     const [state, setState] = useState({
         activeItem: 0,
         filteredItems: [],
@@ -10,8 +11,75 @@ const Autocomplete = props => {
         inputValue: ''
     })
 
-    const [countries, setCountries] = useState(props.countries);
-    const [countriesArray, setCountriesArray] = useState([]);
+    const handleChange = (e) => {
+        const inputValue = e.currentTarget.value;
+        const filteredItems = items.filter(
+            (optionName) => optionName.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
+        );
+
+        setState({
+            activeItem: 0,
+            filteredItems,
+            displayItems: true,
+            inputValue: e.currentTarget.value
+        });
+    };
+
+    const handleClick = (e) => {
+        setState({
+            activeItem: 0,
+            filteredItems: [],
+            displayItems: false,
+            inputValue: e.currentTarget.innerText
+        });
+        fieldRef.current.value = "";
+        handleAddItems(e.currentTarget.innerText)
+    };
+
+    const handleKeyDown = (e) => {
+        const { activeItem, filteredItems } = state;
+
+        if (e.keyCode === 13) {
+            const inList = filteredItems && items.includes(filteredItems[activeItem]);
+            if(!inList || !fieldRef.current.value) return;
+            setState({
+                activeItem: 0,
+                filteredItems: [],
+                displayItems: false,
+                inputValue: filteredItems[activeItem]
+            });
+            fieldRef.current.value = "";
+            handleAddItems(filteredItems[activeItem]);
+        }
+        else if (e.keyCode === 38) {
+            e.preventDefault();
+            if (activeItem === 0) {
+                return;
+            }
+            setState({
+                activeItem: activeItem - 1,
+                filteredItems,
+                displayItems: true,
+                inputValue: e.currentTarget.value
+            });
+        }
+        else if (e.keyCode === 40) {
+            e.preventDefault();
+            if ((filteredItems && activeItem === filteredItems.length - 1) || activeItem >= 9) {
+                return;
+            }
+            setState({
+                activeItem: activeItem + 1,
+                filteredItems,
+                displayItems: true,
+                inputValue: e.currentTarget.value
+            });
+        }
+    };
+
+    const fieldRef = useRef();
+    const [itemsArray, setItemsArray] = useState([]);
+
     const [selected, dispatchSelected] = useReducer((state, action) => {
         switch (action.type) {
             case "add":
@@ -25,86 +93,47 @@ const Autocomplete = props => {
         }
     }, []);
 
-    function handleAddCountries(e) {
+    function handleAddItems(e) {
         dispatchSelected({
             type: "add",
             name: e
         });
     }
 
-    const onChange = (e) => {
-        const inputValue = e.currentTarget.value;
-        const filteredItems = countries.filter(
-            (optionName) => optionName.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
-        );
-
-        setState({
-            activeItem: 0,
-            filteredItems,
-            displayItems: true,
-            inputValue: e.currentTarget.value
-        });
-    };
-
-    const onClick = (e) => {
-        setState({
-            activeItem: 0,
-            filteredItems: [],
-            displayItems: false,
-            inputValue: e.currentTarget.innerText
-        });
-        fieldRef.current.value = "";
-        handleAddCountries(e.currentTarget.innerText)
-    };
-
-    const onKeyDown = (e) => {
-        const { activeItem, filteredItems } = state;
-
-        if (e.keyCode === 13) {
-            const real = countries.includes(filteredItems[activeItem])
-            if(!real || !fieldRef.current.value) return;
-            setState({
-                activeItem: 0,
-                displayItems: false,
-                inputValue: filteredItems[activeItem]
-            });
-            handleAddCountries(filteredItems[activeItem]);
-            fieldRef.current.value = "";
-        }
-    };
-
     useEffect(
         () => {
-            setCountriesArray(selected.map(item => item.name.trim()))
+            setItemsArray(selected.map(item => item.name.trim()))
         }, [selected]
     )
 
     return (
         <>
-            <div>
+            <div className="uk-inline uk-width-1-1 uk-margin-top">
+                <span className="uk-form-icon" data-uk-icon="icon: world" />
                 <input
                     ref={fieldRef}
                     name="languages"
-                    label="Countries"
+                    label="Items"
+                    placeholder="Enter a country and press enter"
                     className="uk-input uk-form-large uk-width-expand"
-                    onChange={onChange}
-                    onKeyDown={onKeyDown}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
                     value={(fieldRef.current && fieldRef.current.value) || ""}
                     autoComplete="off"
                     autoCapitalize="off"
                     autoCorrect="off"
                     />
 
-                {
-                    state.displayItems && state.inputValue.length && state.filteredItems.length ?
-                    <div className="list-panel uk-panel uk-padding-small uk-box-shadow-medium">
+                {state.displayItems && state.inputValue.length && state.filteredItems ?
+                    <div className="list-panel uk-panel uk-padding-remove uk-box-shadow-medium">
                         <ul className="uk-list">
                         {
                             state.filteredItems.map((optionName, index) => {
                                 return (
                                     <li
+                                        className={`${state.activeItem === index ? "active-item" : "default-item"}`}
                                         key = {optionName}
-                                        onClick = {onClick}>
+                                        onClick = {handleClick}>
                                         {optionName}
                                     </li>
                                 )
@@ -117,7 +146,7 @@ const Autocomplete = props => {
             </div>
 
             <div className="tag-container uk-margin-top" data-uk-margin>
-            {countriesArray.map((item, index) => (
+            {itemsArray.map((item, index) => (
                 <div key={`country_${index}`} className="icon-tags uk-label uk-border-pill uk-width-auto uk-margin-small-right">
                     <span data-uk-icon="icon: close; ratio: 1.15" onClick={() => dispatchSelected({ type: "remove", index })} />
                     <p>{item}</p>
