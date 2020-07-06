067091e0ef3f3f8531b6c3e9e2af6e01d49155a1
Michael <michael@pixt.us>
2020-07-06 10:10:32 +0700

    messing with background image

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <section
         className="uk-section uk-light uk-flex uk-flex-center uk-overflow-auto">
-        <div className="uk-padding uk-width-1-2@m">
+        <div className="uk-padding uk-margin-large-bottom uk-width-1-2@m">
             <h1>Where in the world<br />have you been?</h1>
             <Autocomplete countries={countriesList} />
         </div>
